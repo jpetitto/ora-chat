b41329853f69ae67e3dd5b25672fb1e2bcca430d
John Petitto <johnp@walla.by>
2017-07-29 17:36:14 -0700

    Create skeleton for Register screen

--- app/src/main/java/com/johnpetitto/orachat/RegisterFragment.java
+++ app/src/main/java/com/johnpetitto/orachat/RegisterFragment.java
@@ -6,12 +6,32 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class RegisterFragment extends Fragment {
+    @BindView(R.id.register_name) EditText name;
+    @BindView(R.id.register_email) EditText email;
+    @BindView(R.id.register_password) EditText password;
+    @BindView(R.id.register_confirm) EditText confirm;
+    @BindView(R.id.register_button) Button button;
+    private Unbinder unbinder;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_register, container, false);
+        unbinder = ButterKnife.bind(this, rootView);
         return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
