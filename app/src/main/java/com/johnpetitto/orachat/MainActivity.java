1dfc0416df303579908507293aa377833d07bf4d
John Petitto <johnp@walla.by>
2017-07-30 17:45:47 -0700

    Create skeleton for main screen

--- app/src/main/java/com/johnpetitto/orachat/MainActivity.java
+++ app/src/main/java/com/johnpetitto/orachat/MainActivity.java
@@ -1,14 +1,82 @@
 package com.johnpetitto.orachat;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener, BottomNavigationView.OnNavigationItemSelectedListener {
+    private static final int CHAT_POSITION = 0;
+    private static final int ACCOUNT_POSITION = 1;
+
+    @BindView(R.id.main_toolbar) Toolbar toolbar;
+    @BindView(R.id.main_view_pager) ViewPager viewPager;
+    @BindView(R.id.main_navigation) BottomNavigationView navigation;
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        viewPager.setAdapter(new NavigationPagerAdapter(getSupportFragmentManager()));
+        viewPager.addOnPageChangeListener(this);
+        navigation.setOnNavigationItemSelectedListener(this);
     }
+
+    @Override
+    public void onPageSelected(int position) {
+        navigation.getMenu().getItem(position).setChecked(true);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.chat:
+                viewPager.setCurrentItem(CHAT_POSITION);
+                return true;
+            case R.id.account:
+                viewPager.setCurrentItem(ACCOUNT_POSITION);
+                return true;
+        }
+        return false;
+    }
+
+    private class NavigationPagerAdapter extends FragmentPagerAdapter {
+        NavigationPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case CHAT_POSITION:
+                    return new ChatFragment();
+                case ACCOUNT_POSITION:
+                    return new AccountFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+    @Override
+    public void onPageScrollStateChanged(int state) {}
 }
